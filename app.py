b9eeaa3d23ff3d9a68d3c7ee19fe41b11a05a9d6
Aditya Satheesh <66138996+cybersamurai2410@users.noreply.github.com>
2024-03-13 16:25:32 +0530

    Implemented functions for text extraction, chunk splitting and vector storage.

--- app.py
+++ app.py
@@ -1,15 +1,72 @@
 from dotenv import load_dotenv
 import streamlit as st 
 from PyPDF2 import PdfReader
+from enum import Enum 
 
 from langchain_google_genai import GoogleGenerativeAI
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.prompts import PromptTemplate
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
-llm = ChatGoogleGenerativeAI(model="gemini-pro")
+from langchain.prompts import PromptTemplate
+from langchain_text_splitters import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS 
+
+llm = ChatGoogleGenerativeAI(model="gemini-pro") # Create dictionary of LLMs  
+Documents = Enum('Doc_Type', ['PDF', 'WEBPAGE', 'YOUTUBE', 'TEXT', 'CSV'])
+
+# 1 token ~= 4 chars in English; cost = estimate_tokens * price per token
+def estimate_tokens(text, token_cost):
+    token_size = len(text)/4
+    cost = token_size*token_cost
+    return token_size, cost
+
+#---------------------
+# Document Q&A
+def document_loader():
+    '''
+    Documents:
+        - PDF (.pdf)
+        - Webpage 
+        - YouTube
+        - Text (.txt)
+        - CSV (.csv)
+    '''
+    pass
+
+# SQL Q&A
+def database_loader():
+    pass
+
+def image_loader():
+    pass
+
+#---------------------
 
 def extract_document(docs):
-    pass
+    text = ""
+    for pdf in docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+    return text
+
+def get_text_chunks(text):
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len 
+    )
+    chunks = text_splitter.split_text(text)
+
+    return chunks
+
+def get_vectorstore(chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
+
+    return vectorstore
 
 def main():
     load_dotenv()
@@ -29,8 +86,10 @@ def main():
                 raw_text = extract_document(docs) 
 
                 # Retrieve text chunks
+                text_chunks = get_text_chunks(raw_text)
 
                 # Create vector store
+                vectorstore = get_vectorstore(text_chunks)
 
 if __name__ == '__main__':
     main()
