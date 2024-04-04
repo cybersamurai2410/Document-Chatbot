0d3e1817dbaa3f16be6306c809f16ff28767d14a
Aditya Satheesh <66138996+cybersamurai2410@users.noreply.github.com>
2024-04-04 13:27:29 +0530

    Modified architecture of user interface and functionalities.

--- app.py
+++ app.py
@@ -1,163 +1,112 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import Chroma
+
+from models import llms, embeddings
 from dotenv import load_dotenv
 import time
-import random 
-import streamlit as st 
-from PyPDF2 import PdfReader
-from enum import Enum 
+import os 
+from io import BytesIO
+import tempfile
 
-# LLMs and Embeddings
-from models import llm, embedding
+load_dotenv()
+llm = llms['gemini-pro']
+embedding = embeddings["gemini-pro"]
 
-# from langchain.chat_models import ChatOpenAI
-# from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
-
-from langchain.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
-from langchain_text_splitters import CharacterTextSplitter, RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import FAISS 
-from langchain.memory import ConversationBufferMemory, ChatMessageHistory
-from langchain.chains import ConversationalRetrievalChain, StuffDocumentsChain, LLMChain
-
-Documents = Enum('Doc_Type', ['PDF', 'WEBPAGE', 'YOUTUBE', 'TEXT', 'CSV'])
-
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
-# 1 token ~= 4 chars in English; cost = estimate_tokens * price per token
-def estimate_tokens(text, token_cost):
-    token_size = len(text)/4
-    cost = token_size*token_cost
-    return token_size, cost
-
-#---------------------
-# Document Q&A
-def document_loader(doc_type=0):
-    '''
-    Documents:
-        - PDF (.pdf)
-        - Webpage 
-        - YouTube
-        - Text (.txt)
-        - CSV (.csv) -> integrate agents for data science with dataframe
-    '''
-    pass
-
-# SQL Q&A
-def database_loader():
-    pass
-
-def image_loader():
-    pass
-
-#---------------------
-
-def extract_document(docs):
-    text = ""
-    for pdf in docs:
-        pdf_reader = PdfReader(pdf)
-        for page in pdf_reader.pages:
-            text += page.extract_text()
-
-    return text
-
-def get_text_chunks(text):
-    text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len 
-    )
-    chunks = text_splitter.split_text(text)
-
-    return chunks
-
-def get_vectorstore(chunks):
-    embeddings = embedding["gemini-pro"]
-    vectorstore = FAISS.from_texts(texts=chunks, embedding=embeddings)
-
-    return vectorstore
-
-def get_conversation_chain(vectorstore):
-    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
-    conversation_chain = ConversationalRetrievalChain.from_llm(
-        llm = llm['gemini-pro'],
-        retriever=vectorstore.as_retriever(),
-        memory=memory 
-    )
-
-    return conversation_chain
-
-def get_response(user_prompt):
-    response = st.session_state.conversation({"question": user_prompt}) # after executing conversation = get_conversation_chain; question, chat_history and answer key from output 
-    st.session_state.chat_history = response['chat_history']
-
-    print(response, '\n')
-    '''
-    {'question': 'what is this document about?', 'chat_history': [HumanMessage(content='what is this document about?'), 
-    AIMessage(content='I do not know the answer to this question. The provided text does not contain any information about what the document is about.')], 
-    'answer': 'I do not know the answer to this question. The provided text does not contain any information about what the document is about.'}
-    '''
-
-    for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0: # Even numbers are user messages else AI messages
-            with st.chat_message("user"):
-                st.markdown(message.content)
-        else:
-            with st.chat_message("assistant"):
-                st.write_stream(stream_response(message.content))
-
-def stream_response(message):
-    for word in message.split():
-        yield word + " "
-        time.sleep(0.05)
-
-
-def main():
-    load_dotenv()
-
-    st.set_page_config(page_title="Chat with your documents", page_icon=":books:")
-
-    # st.session_state prevents variables from reinitializing since streamlit often reloads code during session
-    if "conversation" not in st.session_state:
-        st.session_state.conversation = None
+def chat(prompt, docs):
     if "chat_history" not in st.session_state:
-        st.session_state.chat_history = None
+        st.session_state.chat_history = []
 
-    st.header("Chat with your documents :books:")
-    # st.text_input("Ask a question:")
-    user_prompt = st.chat_input("Ask a question about your documents")
-    if user_prompt:
-        get_response(user_prompt)
+    for message in st.session_state.chat_history:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-    # # Display chat messages from history on app rerun
-    # for message in st.session_state.chat_history:
-    #     with st.chat_message(message["role"]):
-    #         st.markdown(message["content"])
+    def stream_response(message):
+        for word in message.split():
+            yield word + " "
+            time.sleep(0.05)
 
-    # if prompt := st.chat_input("Ask a question about your documents"):
-    #     st.session_state.chat_history.append({"role": "user", "content": prompt}) # Add user message to chat history
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    
+    with st.chat_message("assistant"):
+        if docs:
+            response = st.write_stream(stream_response(prompt)) 
 
-    #     with st.chat_message("user"):
-    #         st.markdown(prompt)
-    #     with st.chat_message("assistant"):
-    #         response = st.write_stream(stream_response())
-    #     st.session_state.chat_history.append({"role": "assistant", "content": response}) # Add assistant response to chat history
+            st.session_state.chat_history.append({"role": "user", "content": prompt})
+            st.session_state.chat_history.append({"role": "assistant", "content": response})
+        else:
+            st.write_stream(stream_response("Please upload your documents."))
+    
+    print("Chat history: ", st.session_state.chat_history)
 
-    with st.sidebar:
-        st.subheader("Your documents")
-        docs = st.file_uploader("Upload your documents here and click on 'Process'", accept_multiple_files=True)
+def document_loader(docs):
+    merge_docs = []
+    for file in docs:
+        temp_file_descriptor, temp_file_path = tempfile.mkstemp(suffix='.pdf')
+        os.close(temp_file_descriptor)
+
+        with open(temp_file_path, 'wb') as temp_file:
+            temp_file.write(file.getvalue())
+
+        loader = PyPDFLoader(temp_file_path)  
+        documents = loader.load_and_split()
+        documents = documents[:3] 
+        merge_docs.extend(documents)
+        os.remove(temp_file_path)
+
+    print(merge_docs)
+    vectorstore = Chroma.from_documents(documents, embedding)
+    retriever = vectorstore.as_retriever()
+
+    return retriever
+
+st.set_page_config(page_title="Document Chatbot", page_icon="✨")
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Main Menu",
+        options=["PDF", "CSV", "SQL", "Webpage", "YouTube"],
+        icons=["filetype-pdf", "filetype-csv", "filetype-sql", "link", "youtube"], # https://icons.getbootstrap.com/
+        default_index=0,
+    )
+
+    if selected == "PDF":
+        st.title(f"Chat with {selected}")
+        docs = st.file_uploader(f"Upload your {selected} files here and click on **Process**", accept_multiple_files=True)
+
+        if docs is not None:
+            docs = [doc for doc in docs if doc.type == "application/pdf"]
         
-        if st.button("Process"):
-            with st.spinner("Processing"):
-                raw_text = extract_document(docs) # Extract document text
-                text_chunks = get_text_chunks(raw_text) # Retrieve text chunks
-                print(text_chunks)
-                vectorstore = get_vectorstore(text_chunks) # Create vector store
-                print(vectorstore)
-                st.session_state.conversation = get_conversation_chain(vectorstore) # Create conversation chain
+        # Show names of files once processed
+        try: 
+            if st.button("Process"):
+                with st.spinner("Processing"):
+                    print(docs)
+                    document_loader(docs)
+                
+                success = st.success("Files processed successfully")
+                time.sleep(3)
+                success.empty()
 
-if __name__ == '__main__':
-    main()
+        except Exception as e:
+            error = st.error(f"Error uploading files:\n {str(e)}")
 
+st.title("Document Chatbot 📚🤖")
+if prompt := st.chat_input("Ask anything..."):
+    chat(prompt, docs)
 
-# streamlit run app.py
-# https://docs.streamlit.io/knowledge-base/tutorials/build-conversational-apps
+if selected == "CSV":
+    st.title(f"Chat with {selected}")
+if selected == "SQL":
+    st.title(f"Chat with {selected}")
+if selected == "Webpage":
+    st.title(f"Chat with {selected}")
+if selected == "YouTube":
+    st.title(f"Chat with {selected}")
+
+# streamlit run prototype.py
+# C:\Users\Dell\AppData\Local\Temp\tmpwc1bhv2v.pdf
+    
