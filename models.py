0280e6633d7364a93099e4e21c2da8c2b0df9cd1
Aditya Satheesh <66138996+cybersamurai2410@users.noreply.github.com>
2024-04-15 17:06:28 +0530

    updated models

--- models.py
+++ models.py
@@ -5,17 +5,19 @@ from langchain_community.embeddings import HuggingFaceEmbeddings, HuggingFaceInf
 
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
-from langchain_google_genai import GoogleGenerativeAI
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAI, ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_cohere import ChatCohere, CohereEmbeddings # CohereRagRetriever, CohereRerank
 
 llms = {
     "gemini-pro": ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True),
+    "claude-3-sonnet": ChatAnthropic(model_name="claude-3-sonnet-20240229", temperature=0),
+    "cohere": ChatCohere(model="command", temperature=0), # command-r-plus
     "llama2": ChatOllama(model="llama2", temperature=0),
 }  
 
 embeddings = {
     "gemini-pro": GoogleGenerativeAIEmbeddings(model="models/embedding-001"),
+    "cohere": CohereEmbeddings(model="embed-english-v3.0"), # embed-multilingual-v3.0 
     "llama2": OllamaEmbeddings(model="llama2"),
 }
 
