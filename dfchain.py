bf3126bb8a477932b12c3b1bdde91a57971989ec
Aditya Satheesh <66138996+cybersamurai2410@users.noreply.github.com>
2024-04-19 20:32:23 +0530

    refactoring

--- dfchain.py
+++ dfchain.py
@@ -1,8 +1,9 @@
 from operator import itemgetter
 
 from langchain import hub
-from langchain.agents import AgentExecutor, AgentType, Tool, tool, load_tools, create_structured_chat_agent, create_react_agent
-from langchain.tools.render import render_text_description
+from langchain.agents import AgentExecutor, AgentType, Tool, tool, create_structured_chat_agent, create_react_agent
+from langchain.tools import BaseTool
+from langchain.tools.render import render_text_description_and_args
 from langchain_experimental.tools import PythonAstREPLTool
 
 from langchain_core.prompts import ChatPromptTemplate, PromptTemplate, MessagesPlaceholder
@@ -10,11 +11,12 @@ from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough, RunnableParallel
 
 @tool
-def get_dfchain(dataframes):
+def get_dfchain(dataframes, llm):
     """Execute python code using pandas datframe."""
 
     pytool = PythonAstREPLTool(locals=dataframes)
     # tools = [pytool]
+    # llm_with_tools = llm.bind(tools)
 
     df_template = """```python
     {df_name}.head().to_markdown()
@@ -27,22 +29,50 @@ def get_dfchain(dataframes):
     )
     print(df_context)
 
-    chain = ''
-    solution = chain.invoke()
+    system = f"""You have access to a number of pandas dataframes. \
+    Here is a sample of rows from each dataframe and the python code that was used to generate the sample:
 
-    return solution
+    {df_context}
+
+    Given a user question about the dataframes, write the Python code to answer it. \
+    Don't assume you have access to any libraries other than built-in Python ones and pandas. \
+    Make sure to refer only to the variables mentioned above."""
+    prompt = ChatPromptTemplate.from_messages([("system", system), ("human", "{question}")])
+
+    explainsolution_prompt = PromptTemplate.from_template(
+        """Tell what the answer is first then explain briefly what calculation was used and provide analysis of the answer. \
+            Do not mention any coding syntax terms or variable names from the code. 
+        
+        Code:
+        {code}
+        
+        Answer:
+        {answer}"""
+    )
+
+    code_chain = prompt | llm | StrOutputParser()
+    solution_chain = (
+    RunnablePassthrough.assign(code=code_chain)
+    .assign(answer=itemgetter("code") | pytool)
+    .assign(explanation = {"code": itemgetter("code"), "answer": itemgetter("answer")} | explainsolution_prompt | llm) 
+    .pick(["code", "answer", "explanation"])
+    )
+    # solution = solution_chain.invoke({"question": question})
+
+    return solution_chain
 
 @tool
-def df_stats(datframes):
+def df_stats(dataframes):
     """Get statistics of pandas dataframe."""
     pass
 
+print("Executing chain...")
 csv_tools = [get_dfchain]
-rendered_tools = render_text_description(csv_tools)
-prompt = hub.pull("hwchase17/react")
+rendered_tools = render_text_description_and_args(csv_tools)
+prompt = hub.pull("hwchase17/react-json") # react-multi-input-json
 
 def execute_csvagent(llm, question):
-    agent = create_react_agent(prompt, llm, csv_tools) # ReAct - Synergizing Reasoning and Acting in Language Models
+    agent = create_react_agent(prompt, llm, csv_tools) 
     agent_executor = AgentExecutor(agent=agent, tools=csv_tools, verbose=True, max_iterations=5)
     result = agent_executor.invoke({"input": question})
 
