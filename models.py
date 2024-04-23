34f8776911c34877910c6eb2b0328d85625e7091
Aditya Satheesh <66138996+cybersamurai2410@users.noreply.github.com>
2024-04-23 22:02:41 +0530

    update models

--- models.py
+++ models.py
@@ -7,6 +7,7 @@ from langchain_openai import ChatOpenAI # Also used for TogetherAI
 from langchain_anthropic import ChatAnthropic
 from langchain_google_genai import GoogleGenerativeAI, ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_cohere import ChatCohere, CohereEmbeddings # CohereRagRetriever, CohereRerank
+from langchain_groq import ChatGroq
 # from langchain_mistralai import ChatMistralAI
 # from langchain_google_vertexai import ChatVertexAI
 # from langchain_fireworks import ChatFireworks
@@ -15,17 +16,22 @@ llms = {
     "gemini-pro": ChatGoogleGenerativeAI(model="gemini-pro", convert_system_message_to_human=True),
     "claude-3-sonnet": ChatAnthropic(model_name="claude-3-sonnet-20240229", temperature=0),
     "cohere": ChatCohere(model="command", temperature=0), # command-r-plus
-    "llama2": ChatOllama(model="llama2", temperature=0),
-    "gpt-3.5": 'ChatOpenAI(temperature=0)'
+    "groq-llama3_8b": ChatGroq(temperature=0, model_name="llama3-8b-8192"),
+    "groq-gemma7b": ChatGroq(temperature=0, model_name="gemma-7b-it"),
+    "gpt-3.5-turbo": 'ChatOpenAI(temperature=0)'
 }  
 
 embeddings = {
     "gemini-pro": GoogleGenerativeAIEmbeddings(model="models/embedding-001"),
     "cohere": CohereEmbeddings(model="embed-english-v3.0"), # embed-multilingual-v3.0 
-    "llama2": OllamaEmbeddings(model="llama2"),
 }
 
 '''
-"hf_gemma-7b-it": HuggingFaceHub(repo_id="google/gemma-7b-it")
-"hf_gemma-7b-it": HuggingFaceEmbeddings()
+local models:
+"llama2": ChatOllama(model="llama2", temperature=0),
+"llama2": OllamaEmbeddings(model="llama2"),
+
+huggingface:
+"hf_gemma-7b-it": HuggingFaceHub(repo_id="google/gemma-7b-it"),
+"hf_gemma-7b-it": HuggingFaceEmbeddings(),
 '''
