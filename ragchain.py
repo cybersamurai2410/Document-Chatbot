c7a16ff6f7f2dcc975000addc82a1eccc6d7d95e
Aditya Satheesh <66138996+cybersamurai2410@users.noreply.github.com>
2024-04-27 03:13:38 +0530

    refactoring

--- ragchain.py
+++ ragchain.py
@@ -4,6 +4,7 @@ from langchain.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceh
 from langchain_core.prompts import format_document
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, get_buffer_string
 from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough, RunnableParallel, Runnable
 
 DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
 def combine_documents(
@@ -21,18 +22,20 @@ Follow Up Input: {question}
 Standalone question:""" 
 CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(template)
 
-template = """Answer the question in a few sentences based on the following context if it is relevant or else use your own knowledge to answer the question.
+template = """Answer the question in a few sentences based on the following context if it is relevant or else use your own knowledge to answer the question and mention that the context was not relevant.
 
 Context:
 {context}
 
 Question: {question}
+
+These are the names of the files you have access to where context is retrieved: {files}
 """
 ANSWER_PROMPT = ChatPromptTemplate.from_template(template) 
 
 # RAG Chain for reading PDF files
-def get_ragchain(loaded_memory, retriever, llm):
-    # chat_history = lambda x: get_buffer_string(x["chat_history"])
+def get_ragchain(loaded_memory, retriever, llm, files):
+    print(llm)
 
     standalone_question = {
     "standalone_question": {
@@ -47,17 +50,18 @@ def get_ragchain(loaded_memory, retriever, llm):
     retrieved_documents = {
         "docs": itemgetter("standalone_question") | retriever, # Retrieve list of sources passing just standalone question to retriever
         "question": lambda x: x["standalone_question"]
-        # "chat_history": itemgetter("chat_history")
     }
 
     answer = {
         "answer": {
             "context": lambda x: combine_documents(x["docs"]), # x is dictionary from retrieved_documents with key passed as parameter to return context
-            "question": itemgetter("question") # Gets question key from retrieved_documents dictionary 
+            "question": itemgetter("question"), # Gets question key from retrieved_documents dictionary 
+            "files": RunnablePassthrough(lambda *args: files)
             } | ANSWER_PROMPT | llm,
         "docs": itemgetter("docs")
     }
 
+    # Only runnables and dicts can be passed in pipeline
     chain = loaded_memory | standalone_question | retrieved_documents | answer
 
     return chain
