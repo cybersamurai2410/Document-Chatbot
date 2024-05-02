7bbad544900612017b62972a0d52e90dd034417e
Aditya Satheesh <66138996+cybersamurai2410@users.noreply.github.com>
2024-05-03 00:27:24 +0530

    Implemented retriever tool for ragagent function.

--- urlchain.py
+++ urlchain.py
@@ -3,11 +3,18 @@ from operator import itemgetter
 from langchain.agents import AgentExecutor, AgentType, Tool, tool
 from langchain_community.tools import DuckDuckGoSearchResults
 
+from langchain_core.documents import Document
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import (
+    create_history_aware_retriever,
+    create_retrieval_chain,
+)
+
 from langchain.prompts import PromptTemplate, ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.prompts import format_document
-from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, get_buffer_string
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough, RunnableParallel, Runnable
+from langchain.tools.retriever import create_retriever_tool
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -21,10 +28,21 @@ asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 # RAG Agent 
 def get_ragagent(loaded_memory, llm, retriever):
     
-    template = """"""
+    prompt = ""
+
+    tools = []
+    retriever_tool = create_retriever_tool(
+    retriever = retriever,
+    name = "webpage title",
+    description = "summary of webpage"
+    )
+    tools.append(retriever_tool) # Loop retriever for different groups of urls then add to tools list
+
+    # agent = create_openai_tools_agent(llm, tools, prompt)
+    # agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    # return agent_executor
 
 def websearch_chain(llm):
-
     # search = GoogleSearchAPIWrapper(k=1)
     search = DuckDuckGoSearchResults()
     search_tool = Tool(
@@ -50,11 +68,13 @@ def websearch_chain(llm):
 
     return chain 
 
+def youtube_chain(llm, retreiver):
+    pass
+
 # question = "what are the results of the last ufc event?"
 # chain = websearch_chain(llm)
 # result = chain.invoke({"question": question})
 # print(result)
-
-'''
+"""
 [snippet: From a perfect uppercut in the welterweight opener to a massive right hook that ended the evening, the first event after UFC 300 was an explosive affair that produced plenty of highlights and gave ..., title: Main Card Results | UFC Fight Night: Nicolau vs Perez, link: https://www.ufc.com/news/main-card-results-highlights-winner-interviews-ufc-fight-night-nicolau-vs-perez?language_content_entity=en], [snippet: See The Fight Results, Watch Post-Fight Interviews With The Main Card Winners And More From UFC 298: Volkanovski vs Topuria, Live From Honda Center In Anaheim By E. Spencer Kyte, on X @spencerkyte ..., title: Main Card Results | UFC 298: Volkanovski vs Topuria - UFC.com, link: https://www.ufc.com/news/main-card-results-highlights-winner-interviews-ufc-298-volkanovski-vs-topuria?language_content_entity=en], [snippet: Ultimate Fighting Championship (UFC) was back at the friendly confines of the Apex last night (Sat., April 27, 2024) in Las Vegas, Nevada for UFC Vegas 91. Headlining the event was a Flyweight ..., title: UFC Vegas 91 results: Biggest winners, loser from 'Nicolau vs. Perez ..., link: https://www.mmamania.com/2024/4/28/24143415/ufc-vegas-91-results-biggest-winners-loser-nicolau-perez-last-night-espn-mma], [snippet: Esteban Ribovics (29-28, 29-28, 29-28) defeats Kamuela Kirk by unanimous decision . Esteban Ribovics and Kamuela Kirk set the Fight of the Night bar high right out of the gate on Saturday night ..., title: UFC 290: Volkanovski vs Rodriguez Final Results - UFC.com, link: https://www.ufc.com/news/ufc-290-volkanovski-vs-rodriguez-results-highlights-winner-interviews?language_content_entity=en]
-'''
+"""
