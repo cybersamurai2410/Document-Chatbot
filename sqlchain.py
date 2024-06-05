75969fa348885390595073dd1420db27ccd9c805
Aditya Satheesh <66138996+cybersamurai2410@users.noreply.github.com>
2024-06-06 03:28:32 +0530

    refactoring

--- sqlchain.py
+++ sqlchain.py
@@ -1,9 +1,14 @@
 from langchain_community.utilities import SQLDatabase 
 from langchain_community.agent_toolkits import create_sql_agent
+import mysql.connector 
 
 def init_database(user: str, password: str, host: str, port: str, database: str) -> SQLDatabase:
-    db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
+    db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}" # mysql driver
+    print(db_uri)
 
-    return SQLDatabase.from_uri(db_uri)
+    try:
+        return SQLDatabase.from_uri(db_uri)
+    except Exception as e:
+        print(f"An error occurred while trying to connect to the database: {e}")
 
 # agent_executor = create_sql_agent(llm, db=db, agent_type="tool-calling", verbose=True)
