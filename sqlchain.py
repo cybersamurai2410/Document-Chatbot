99f8b2518a7e73bd393b9c8c08afdcc0fef9cbbf
Aditya Satheesh <66138996+cybersamurai2410@users.noreply.github.com>
2024-06-18 16:52:22 +0530

    sql chain generating query and response

--- sqlchain.py
+++ sqlchain.py
@@ -2,6 +2,10 @@ from langchain_community.utilities import SQLDatabase
 from langchain_community.agent_toolkits import create_sql_agent
 import mysql.connector 
 
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+
 def init_database(user: str, password: str, host: str, port: str, database: str) -> SQLDatabase:
     db_uri = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}" # mysql driver
     print(db_uri)
@@ -10,5 +14,59 @@ def init_database(user: str, password: str, host: str, port: str, database: str)
         return SQLDatabase.from_uri(db_uri)
     except Exception as e:
         print(f"An error occurred while trying to connect to the database: {e}")
+
+def get_sqlchain(db, llm):
+    template = """
+    You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
+    Based on the table schema below, write a SQL query that would answer the user's question. Take the conversation history into account.
+    
+    <SCHEMA>{schema}</SCHEMA>
+    
+    Conversation History: {chat_history}
+    
+    Write only the SQL query and nothing else. Do not wrap the SQL query in any other text, not even backticks.
+    
+    For example:
+    Question: which 3 artists have the most tracks?
+    SQL Query: SELECT ArtistId, COUNT(*) as track_count FROM Track GROUP BY ArtistId ORDER BY track_count DESC LIMIT 3;
+    Question: Name 10 artists
+    SQL Query: SELECT Name FROM Artist LIMIT 10;
+    
+    Your turn:
+    
+    Question: {question}
+    SQL Query:
+    """
+    
+    prompt = ChatPromptTemplate.from_template(template)
+
+    def get_schema(_):
+        return db.get_table_info()
+    
+    query_chain = RunnablePassthrough.assign(schema=get_schema) | prompt | llm | StrOutputParser()
+
+    template = """
+    You are a data analyst at a company. You are interacting with a user who is asking you questions about the company's database.
+    Based on the table schema below, question, sql query, and sql response, write a natural language response.
+    <SCHEMA>{schema}</SCHEMA>
+
+    Conversation History: {chat_history}
+    SQL Query: <SQL>{query}</SQL>
+    User question: {question}
+    SQL Response: {response}"""
+  
+    prompt = ChatPromptTemplate.from_template(template)
+
+    chain = (
+        RunnablePassthrough.assign(query=query_chain).assign(
+        schema=lambda _: db.get_table_info(),
+        response=lambda vars: db.run(vars["query"]),
+        )
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    return chain 
 
 # agent_executor = create_sql_agent(llm, db=db, agent_type="tool-calling", verbose=True)
