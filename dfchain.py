7eb4059818c43159a5e8103284f36dabe5ae36d3
Aditya Satheesh <66138996+cybersamurai2410@users.noreply.github.com>
2024-11-06 05:40:38 +0530

    extended chain to explain results and modified prompt to generate graphs

--- dfchain.py
+++ dfchain.py
@@ -1,15 +1,17 @@
 from operator import itemgetter
+import os
+from pydantic import BaseModel, Field
+from typing import List 
 
 from langchain import hub
 from langchain.agents import AgentExecutor, AgentType, Tool, tool
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent, create_csv_agent
-from langchain.tools import BaseTool
 from langchain.tools.render import render_text_description_and_args
 from langchain_experimental.tools import PythonAstREPLTool
 
 from langchain_core.prompts import ChatPromptTemplate, PromptTemplate, MessagesPlaceholder
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
-from langchain_core.output_parsers import StrOutputParser, JsonOutputParser
+from langchain_core.output_parsers import StrOutputParser, JsonOutputParser, PydanticOutputParser
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough, RunnableParallel, Runnable
 
 class DataFrameToolChain:
@@ -22,12 +24,22 @@ class DataFrameToolChain:
     def call_tools(self, msg: AIMessage) -> dict:
         """Simple sequential tool calling helper."""
 
+        print("msg: ", msg)
         tool_map = {tool.name: tool for tool in self.tools} # Dictionary of all tools
         tool_calls = msg.tool_calls.copy() # Copy list of tool calls
-        print(tool_calls)
-
+        
         for tool_call in tool_calls:
             tool_call["output"] = tool_map[tool_call["name"]].invoke(tool_call["args"]) # Execute tool with provided parameters and add to output key in dictionary
+            
+            if tool_call["output"]:
+                prompt = f"""Provide and explanation for the result given the following code:
+                {tool_call["args"]["query"]}
+
+                Result:
+                {tool_call["output"]}
+                
+                The answer should explain the reasoning to get the result and not explain the code syntax."""
+                tool_call["explanation"] = self.llm.invoke(prompt)
         
         return tool_calls
 
@@ -42,16 +54,20 @@ class DataFrameToolChain:
             df_template.format(df_head=df.head().to_markdown(), df_name=df_name)
             for df_name, df in self.dataframes.items()
         )
-        print(df_context)
+        # print(df_context)
 
         system = f"""You have access to a number of pandas dataframes. \
         Here is a sample of rows from each dataframe and the python code that was used to generate the sample:
 
         {df_context}
 
-        Given a user question about the dataframes, write the Python code to answer it. \
-        Don't assume you have access to any libraries other than built-in Python ones and pandas. \
-        Make sure to refer only to the variables mentioned above."""
+        Write the Python code to answer the user prompt that can be requesting data analytics and visualizations about the dataframes. \
+        You already have access to the dataframes as local variables: {self.dataframes.keys()}. \
+        You can use the library matplotlib for generating graphs if data visualization is relevant. \
+        Give the graph a unique file name then save to directory called 'Graphs' e.g. plt.savefig('Graphs/file_name.png') and attach to final answer in markdown format. \
+        This is the current list of image files in the directory 'Graphs': {os.listdir('Graphs')}
+
+        Return the final answer from the code as python dictionary about the results.""" 
         
         prompt = ChatPromptTemplate.from_messages(
             [
@@ -59,14 +75,14 @@ class DataFrameToolChain:
                 MessagesPlaceholder(variable_name="chat_history"),
                 ("human", "{question}")
             ]
-        )
+        ) # .partial(format_instructions=parser.get_format_instructions())
 
         chain = prompt | self.llm_with_tools | self.call_tools
 
         return chain
 
 # chain = DataFrameToolChain(dataframes, llm)
-# result = chain.invoke({"question":xyz, "chat_history":memory.load_memory_variables({})})
+# result = chain.invoke({"question":xyz, "chat_history":memory.load_memory_variables({})["history"]})
 
 def df_agent(llm, paths, question):
     agent = create_csv_agent(
